bedf288eb860900aae8019dd4600276445b3651f
AriHarbo <arielharbo.219@gmail.com>
2025-02-05 13:39:20 -0300

    Slider casi terminado, falta arreglar algo de la flecha de la derecha

--- src/components/GameCardContainer.jsx
+++ src/components/GameCardContainer.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import GameCard from './GameCard'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const GameCardContainer = () => {
 
@@ -13,14 +16,25 @@ const GameCardContainer = () => {
     }, []);
     console.log(games)
 
+    //Settings para que funcione el slider de la libreria
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      };
+
     return (
-        <>
+        <div style={{ maxWidth: "100%", overflow: "hidden", padding: "15px 50px", margin: "0 10rem" }}>
+               <Slider {...settings}>
             {games?.map((e) => {
                 return (
-                    <GameCard key={e.id} game={e} />
+                    <GameCard key={e.id} game={e} style={{ padding: "0 10px" }}/>
                 )
             })}
-        </>
+            </Slider>
+        </div>
     )
 }
 
